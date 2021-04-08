b454fcf8537dbd94956f32e4e14b70c40144591d
Yahya YILDIRIM <63667498+yahyayildirim@users.noreply.github.com>
2021-04-09 00:54:19 +0300

    Update java_ayarlama.sh

--- java_ayarlama.sh
+++ java_ayarlama.sh
@@ -10,66 +10,49 @@ cat << EOF
 #############################################
 # Oracle Java 8 Kurulumu ve Ayarlaması      #
 # Pardus 19 Sürümleri için hazırlanmıştır.  #
-# Hazırlayan: Yahya YILDIRIM                #
 # http://github.com/yahyayildirim           #
 #############################################
 EOF
 
-echo "Oracle Java 8 indiriliyor."
-sudo apt install oracle-java8-jdk 
+echo "Eski java klasörleri kaldırılıyor..."
+rm -rf /usr/lib/jvm/*
 
-echo "JAVA_HOME ayarlanıyor..."
-export JAVA_HOME="/usr/lib/jvm/oracle-java8-jdk-amd64"
+echo "Eğer yüklü değilse, Oracle Java 8 indirilecek ve kurulacak..."
+sudo apt install -f --reinstall oracle-java8-jdk 
 
 
-set java jar javaws
-for A;
-do
-	echo
-	echo "$A için eski yapılandırma ayarları siliniyor..."; sleep 1
-	sudo update-alternatives --remove-all "$A"
-	echo "$A için yeni yapılandırma ayarları tanımlanıyor..."; sleep 2
-	sudo update-alternatives --install "/usr/bin/$A" "$A" "/usr/lib/jvm/oracle-java8-jdk-amd64/bin/$A" 318
-	echo
-done
-
-echo "Java 8 Web Start için Uygulamalar Menüsüne Kısayol oluşturuluyor..."
+echo -e "\nJava 8 Web Start için uygulamalar menüsünde kısayol oluşturuluyor..."
 
 tee > /usr/share/applications/javaws.desktop << EOF
 [Desktop Entry]
 Encoding=UTF-8
 Name=Java 8 Web Start
 Comment=Java 8 Web Start
-Exec=/usr/lib/jvm/oracle-java8-jdk-amd64/bin/javaws %u
+Exec=/usr/lib/jvm/oracle-java8-jdk-amd64/jre/bin/javaws %u
 Terminal=false
 Type=Application
 Icon=javaws
-Categories=Application;Network;
-MimeType=application/x-java-jnlp-file;
+Categories=Application
+MimeType=application/x-java-jnlp-file
 EOF
 chmod +xr /usr/share/applications/javaws.desktop
 
-echo "Java Ayarları için Uygulamalar Menüsüne Kısayol oluşturuluyor..."
+echo -e "Java Ayarları için uygulamalar menüsünde kısayol oluşturuluyor..."
 tee > /usr/share/applications/javawsviewer.desktop << EOF
 [Desktop Entry]
 Encoding=UTF-8
 Name=Java Ayarları
 Comment=Java Ayarları
-Exec=javaws -viewer
+Exec=jcontrol
 Terminal=false
 Type=Application
 Icon=javaws
-Categories=Application;Network;
+Categories=Application
 EOF
 chmod +xr /usr/share/applications/javawsviewer.desktop
 
-echo -e "\nDeğerli Hocam;"
-echo -e "Uygulamalar menüsünden Java Ayarlarını açın ve aşağıdaki değişiklikleri yapın.\n"; sleep 2
+echo -e "\nDeğerli Hocam; Kurulum tamamlandı, şimdi Uygulamalar menüsünden Java Ayarlarını açın ve aşağıdaki değişiklikleri yapın.\n"; sleep 2
 echo -e "1) Network Setting'e tıkla, en altta Direct connection aktif et, sonra OK ile kapat.\n"; sleep 2
 echo -e "2) Settings'e tıkla, en üstteki tiki kaldır. Sonra Delete Files'e tıkla hepsini işaretle ve OK ve tekrar Ok ile kapat.\n";sleep 2
 echo "3) Mavi renkli olan See the Security Tab'a tıkla, eğer https://kesenek.sgk.gov.tr/ ekli değil ise,"
-echo "   Edit Site List'e tıkla Add tıkla ve https://kesenek.sgk.gov.tr/ ekle ve sırasıyla OK -- Apply -- OK ile ekranı kapat.\n";sleep 2
-echo -e "\n4) Firefox ve Google Chrome'den kesenek sitesine gir, herhangi birisine tıkla açılan ekranda kaydet veya sakla diyerek kaydedin."
-echo "   Sonra indirdiğiniz dosyanın üzerinde sağ tıklayıp özellikler diyin ve Birlikte Aç menüsünden Java 8 Web Start bulup işaretleyin"
-echo "   ve Öntanımlı Olarak Ayarlaya tıklayın ve ekranı kapatın.";sleep 2
-echo -e "\n5) Artık ilgili dosyanın üzerine çift tıklayarak sorunsuz bir şekilde .jnlp dosyalarını açabilirsiniz...\n";
+echo "   Edit Site List'e tıkla Add tıkla ve https://kesenek.sgk.gov.tr/ ekle ve sırasıyla OK -- Apply -- OK ile ekranı kapat.\n"
